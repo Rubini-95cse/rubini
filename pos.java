807b445a0318158c6740aefa302b76e112dcb0c9
Rubini-95cse <rubini.d95@gmail.com>
2016-10-21 12:52:45 +0530

    Add files via upload

---
+++ pos.java
@@ -0,0 +1,11 @@
+class pos
+{
+public static void main(string[] args)
+int a;
+if(a>0)
+system.out.println("positive");
+else if(a<0)
+system.out.println("negative");
+else
+system.out.println("blue");
+}
