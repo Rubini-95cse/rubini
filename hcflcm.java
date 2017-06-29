91dff73ad06fc9a0b3f20e252106260073b1c914
Rubini-95cse <rubini.d95@gmail.com>
2017-06-29 14:34:08 +0530

    Add files via upload

---
+++ hcflcm.java
@@ -0,0 +1,30 @@
+package rubi;
+
+import java.util.Scanner;
+
+public class hcflcm {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int temp1,temp2;
+		int gcd,t,lcm;
+		  temp1 = a;
+	        temp2 = b;
+
+	        while (temp2 != 0) {
+	            t = temp2;
+	            temp2 = temp1 % temp2;
+	            temp1 = t;
+	        }
+
+	        gcd = temp1;
+
+	        lcm = (a * b) / gcd;
+	        System.out.println("GCD = " + gcd);
+	        System.out.println("LCM = " + lcm);
+	    }
+	}
+	
+
