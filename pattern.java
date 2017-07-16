030946bc6ca3d6f783443931c3b45b19198cf637
Rubini-95cse <rubini.d95@gmail.com>
2017-07-16 14:59:15 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.Scanner;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			for(int k=0;k<n;k++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	}
+
