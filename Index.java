5da02f866f60b820cd9b428ebce19aeb18ec3a0f
Rubini-95cse <rubini.d95@gmail.com>
2017-07-22 12:10:59 +0530

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,21 @@
+package rubi;
+
+import java.util.Scanner;
+
+public class Index {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++){
+          if(a[i]==i){
+        	  System.out.println("The number "+a[i]+" is equal to "+i);
+          }
+          
+	   }
+	}
+}
