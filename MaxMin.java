3801817df73f150b10a56a19a5b9394a6cb96002
Rubini-95cse <rubini.d95@gmail.com>
2017-08-17 21:28:24 +0530

    Add files via upload

---
+++ MaxMin.java
@@ -0,0 +1,17 @@
+package rubi1;
+import java.util.*;
+public class MaxMin {
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		int sum = 0;
+		for(int i = 0 ; i < n ; i++) {
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		sum +=arr[0]+arr[n-1];
+		System.out.println(sum);
+	}
+
+}
